050efc42935b48163481e106330e16a635378b5b
miikuru002 <jamutaqortegavelez@gmail.com>
2026-05-15 01:38:40 -0500

    refactor: remove rubric score controller, service, and related Swagger documentation

--- src/modules/evaluation/rubric-scores/rubric-scores.module.ts
+++ src/modules/evaluation/rubric-scores/rubric-scores.module.ts
@@ -3,13 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { RubricScoreEntity } from './model/rubric-scores.entity';
 import { RubricScoreRepository } from './core/rubric-scores.repository';
-import { RubricScoreService } from './api/rubric-scores.service';
-import { RubricScoreController } from './api/rubric-scores.controller';
 
 @Module({
 	imports: [TypeOrmModule.forFeature([RubricScoreEntity])],
-	controllers: [RubricScoreController],
-	providers: [RubricScoreService, RubricScoreRepository],
-	exports: [RubricScoreService, RubricScoreRepository],
+	controllers: [],
+	providers: [RubricScoreRepository],
+	exports: [RubricScoreRepository],
 })
 export class RubricScoreModule {}
