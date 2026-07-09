b9ec3004235be5aed063dcd699c5c0ba47b75d65
miikuru002 <jamutaqortegavelez@gmail.com>
2026-07-08 23:15:50 -0500

    refactor(rubric-scores): remove unused routes, validation, and repository files

--- src/modules/evaluation/rubric-scores/rubric-scores.module.ts
+++ src/modules/evaluation/rubric-scores/rubric-scores.module.ts
@@ -2,12 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { RubricScoreEntity } from './model/rubric-scores.entity';
-import { RubricScoreRepository } from './core/rubric-scores.repository';
 
 @Module({
 	imports: [TypeOrmModule.forFeature([RubricScoreEntity])],
-	controllers: [],
-	providers: [RubricScoreRepository],
-	exports: [RubricScoreRepository],
 })
 export class RubricScoreModule {}
